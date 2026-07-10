abe82b8603028c1760a1c88eb59a474729e5e611
Captain-XuCX <c121108swer@gmail.com>
2026-07-10 17:44:43 +0800

    fix: 添加 useMemo 导入 + 设置 Nitro vercel 预设

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useMemo } from "react";
 import {
   Radar as RadarIcon,
   Upload,
